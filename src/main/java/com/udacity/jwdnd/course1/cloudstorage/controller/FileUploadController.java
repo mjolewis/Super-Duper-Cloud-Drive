afdcec17f272ebe026a09a734e9aae5edbd1593d
Michael Lewis <mjolewis@gmail.com>
2021-05-19 21:35:32 -0400

    FEAT: Added file feature.
    Allows users to upload, view, and delete files

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileUploadController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileUploadController.java
@@ -3,16 +3,22 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import com.udacity.jwdnd.course1.cloudstorage.model.SuperDuperFile;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.service.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.service.ResponseService;
 import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
 import com.udacity.jwdnd.course1.cloudstorage.service.ValidationService;
+import org.apache.tomcat.util.http.fileupload.InvalidFileNameException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.text.StringEscapeUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**********************************************************************************************************************
- *
+ * Handles client requests to insert and delete files.
  *
  * @author Michael Lewis
  *********************************************************************************************************************/
@@ -22,11 +28,51 @@ public class FileUploadController {
     private final UserService userService;
     private final FileService fileService;
     private final ValidationService validationService;
+    private final ResponseService responseService;
 
-    public FileUploadController(UserService userService, FileService fileService, ValidationService validationService) {
+    public FileUploadController(UserService userService, FileService fileService,
+                                ValidationService validationService, ResponseService responseService) {
         this.userService = userService;
         this.fileService = fileService;
         this.validationService = validationService;
+        this.responseService = responseService;
+    }
+
+    @PostMapping()
+    public String uploadFile(Authentication authentication, @RequestParam("fileUpload") MultipartFile multiPartFile, Model model) {
+        User user = getCurrentUser(authentication);
+        SuperDuperFile superDuperFile = fileService.prepareFileForUpload(multiPartFile, user);
+
+        boolean result = validationService.validate(superDuperFile, user, model, "insert");
+        if (result) {
+            try {
+                result = fileService.insertFile(superDuperFile);
+            } catch (InvalidFileNameException e) {
+                result = false;
+                model.addAttribute("message", e.getMessage());
+            }
+        }
+
+        return responseService.createResponse(result, model, user, fileService);
+    }
+
+    @GetMapping("/download")
+    public void downloadFile(HttpServletResponse response, Authentication authentication,
+                             @ModelAttribute SuperDuperFile file, Model model) throws IOException {
+
+        User currentUser = userService.getUser(authentication.getName());
+        file = fileService.getFile(file.getFileId());
+
+        boolean result = validationService.validate(file, currentUser, model,"download");
+
+        if (result) {
+            response.setContentType(file.getContentType());
+            response.setContentLength(Integer.parseInt(file.getFileSize()));
+            String headerKey = "Content-Disposition";
+            String headerValue = "attachment; filename=" + StringEscapeUtils.escapeHtml4(file.getFileName());
+            response.setHeader(headerKey, headerValue);
+            response.getOutputStream().write(file.getFileData());
+        }
     }
 
     @GetMapping("/delete")
@@ -40,26 +86,10 @@ public class FileUploadController {
             result = fileService.deleteFile(file);
         }
 
-        return "result";
+        return responseService.createResponse(result, model, user, fileService);
     }
 
-    @PostMapping()
-    public String uploadFile(Authentication authentication, @RequestParam("fileUpload") MultipartFile multiPartFile, Model model) {
-        User user = userService.getUser(authentication.getName());
-        SuperDuperFile superDuperFile = fileService.prepareFileForUpload(multiPartFile, user);
-
-//        UploadResponse uploadResponse = fileService.upload(superDuperFile);
-//        switch (uploadResponse) {
-//            case SUCCESS:
-//                model.addAttribute("uploadSuccess", true);
-//            case FAILURE:
-//                model.addAttribute("uploadFailure", false);
-//            case ERROR:
-//                model.addAttribute("uploadError", "Upload error. Please try again.");
-//        }
-
-        // TODO: 5/2/21 return all files
-
-        return "result";
+    private User getCurrentUser(Authentication authentication) {
+        return userService.getUser(authentication.getName());
     }
 }
